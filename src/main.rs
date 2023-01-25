274ce8191a8963c9a93239b3a717ff056c5b360a
Matthew Iannucci <mpiannucci@gmail.com>
2023-01-25 17:56:52 -0500

    Add serde

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use actix_cors::Cors;
 use actix_web::{get, middleware::Logger, App, HttpResponse, HttpServer, Responder};
 
+struct AppState {
+    pub downstream: String,
+}
+
 #[get("/status")]
 async fn status() -> impl Responder {
     HttpResponse::Ok().body("OK")
@@ -18,12 +22,19 @@ async fn main() -> std::io::Result<()> {
         .parse()
         .unwrap();
 
+    let downstream = std::env::var("DOWNSTREAM").expect(
+        "You must specify a downstream WMS server URL with the DOWNSTREAM environment variable.",
+    );
+
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
 
     log::info!("starting rewms server at http://localhost:{bind_port}");
 
-    HttpServer::new(|| {
+    HttpServer::new(move || {
         App::new()
+            .app_data(AppState {
+                downstream: downstream.clone(),
+            })
             .wrap(Logger::default())
             .wrap(Cors::permissive())
             .service(status)
